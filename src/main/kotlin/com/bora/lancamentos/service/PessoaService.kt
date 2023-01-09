fff083fc7b39d75e060ef181944979b3a7e0fede
tqi_tviana <tqi_tviana@uolinc.com>
2023-01-09 14:59:31 -0300

    Colocando url dos correios como propriedade

--- src/main/kotlin/com/bora/lancamentos/service/PessoaService.kt
+++ src/main/kotlin/com/bora/lancamentos/service/PessoaService.kt
@@ -4,17 +4,20 @@ import com.bora.lancamentos.controller.dto.request.CreatePessoaRequest
 import com.bora.lancamentos.model.Endereco
 import com.bora.lancamentos.model.Pessoa
 import com.bora.lancamentos.repository.PessoaRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 
 @Service
 class PessoaService(
     val pessoaRepository: PessoaRepository,
-    val restTemplate: RestTemplate
+    val restTemplate: RestTemplate,
+    @Value("\${integracao.correios.url}")
+    val urlCorreios: String
 ) {
 
     fun criar(dto: CreatePessoaRequest): Pessoa {
-        val endereco = restTemplate.getForObject("https://viacep.com.br/ws/${dto.cep}/json", Endereco::class.java)
+        val endereco = restTemplate.getForObject("$urlCorreios${dto.cep}/json", Endereco::class.java)
         return pessoaRepository.save(Pessoa(
             nome = dto.nome,
             endereco = endereco,
